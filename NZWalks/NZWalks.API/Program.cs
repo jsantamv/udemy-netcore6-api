3ed3b31da993c631bd57108b2d55f0be76f0ffe4
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-01-09 17:04:12 -0600

    Autenticacion

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,5 +1,7 @@
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using NZWalks.API.Data;
 using NZWalks.API.Repositories;
 using NZWalks.API.Repositories.Interfaces;
@@ -11,7 +13,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter a valid JWT bearer token",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    options.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {securityScheme, new string[] {} }
+    });
+});
 
 //We create de Connetion to SQL Server
 builder.Services.AddDbContext<NZWalksDbContext>(option =>
@@ -24,6 +49,10 @@ builder.Services.AddScoped<IWalkRepository, WalksRepository>();
 builder.Services.AddScoped<IWalkRepository,WalksRepository>();
 builder.Services.AddScoped<IWalkDifficultyRepository, WalkDifficultyRepository>();
 builder.Services.AddFluentValidation(options => options.RegisterValidatorsFromAssemblyContaining<Program>());
+
+builder.Services.AddScoped<ITokenHandler, TokenHandler>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 
 
 //Automaper Inject
---
+++ NZWalks/NZWalks.API/Repositories/TokenHandler.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NZWalks.API.Repositories
+{
+    public class TokenHandler : ITokenHandler
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenHandler(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public Task<string> CreateTokenAsync(User user)
+        {
+            // Create Claims
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim(ClaimTypes.Email, user.EmailAddress)
+            };
+
+            // Loop into roles of users
+            user.Roles.ForEach((role) =>
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            });
+
+            SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new(
+                configuration["Jwt:Issuer"],
+                configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+            return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
